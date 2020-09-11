4cc0a4fff9742432b6dc914cad2e801799015f10
MVS Sriharsha <sriharshamvs@gmail.com>
2020-09-11 12:24:17 +0530

    first chart

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import LineChart from "../components/chart.js";
 
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      Hello World!
+      <LineChart />
+    </div>
+  );
 }
