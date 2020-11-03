24f2d7c422fcbdce6ee08b9c1b0d2ab7c533365d
MVS Sriharsha <sriharshamvs@gmail.com>
2020-11-03 13:02:22 +0530

    code refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,11 @@
-import LineChart from "../components/chart.js";
-import { useState, useEffect } from "react";
+import CovidDashboard from "../components/CovidDashboard.js";
 
 const Home = () => {
-  const { covidData } = getData();
   return (
     <div>
-      <h2>Graphical representation of confirmed cases of COVID-19</h2>
-      <LineChart covidData={covidData} />
+      <CovidDashboard />
     </div>
   );
 };
-
-function getData() {
-  const [covidData, setcovidData] = useState([]);
-  useEffect(() => {
-    fetch(`${process.env.API_URL}/covid`)
-      .then((res) => res.json())
-      .then(setcovidData);
-  }, []);
-  return { covidData };
-}
 
 export default Home;
