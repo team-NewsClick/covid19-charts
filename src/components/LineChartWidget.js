69a918fa094dbd11fb639025e8721320230eaeed
MVS Sriharsha <sriharshamvs@gmail.com>
2020-11-05 12:51:50 +0530

    code refactor

--- src/components/LineChartWidget.js
+++ src/components/LineChartWidget.js
@@ -19,78 +19,77 @@ export default function LineChartWidget({ data }) {
   if (data.length == 0) {
     return <div>Loading...</div>
   } else {
-    // const vornoiNodes = [];
-    // const covidData = [...data.covidData];
-    // const [hoveredNode, setHoveredNode] = useState(null);
-    // const [selectedCountries, setSelectedCountries] = useState([
-    //   { value: covidData[0].data.activeCases, label: covidData[0].country },
-    // ]);
+    const vornoiNodes = [];
+    // const covidData = data
+    const [hoveredNode, setHoveredNode] = useState(null);
+    const [selectedCountries, setSelectedCountries] = useState([
+      { value: data[0].data, label: data[0].country },
+    ]);
     
     //   //Name of countries for Select
-    //   const countries = [];
-    //   for (var i = 0; i < covidData.length; i++) {
-    //     countries.push({
-    //       label: covidData[i].country,
-    //       value: covidData[i].data.activeCases,
-    //     });
-    //   }
+      const countries = [];
+      for (var i = 0; i < data.length; i++) {
+        countries.push({
+          label: data[i].country,
+          value: data[i].data,
+        });
+      }
   
     //   //Vornoi Nodes
-    //   for (var i = 0; i < selectedCountries.length; i++) {
-    //     for (var j = 0; j < selectedCountries[i].value.length; j++) {
-    //       vornoiNodes.push({
-    //         x: selectedCountries[i].value[j].x,
-    //         y: selectedCountries[i].value[j].y,
-    //         country: selectedCountries[i].label,
-    //       });
-    //     }
-    //   }
+      for (var i = 0; i < selectedCountries.length; i++) {
+        for (var j = 0; j < selectedCountries[i].value.length; j++) {
+          vornoiNodes.push({
+            x: selectedCountries[i].value[j].x,
+            y: selectedCountries[i].value[j].y,
+            country: selectedCountries[i].label,
+          });
+        }
+      }
   
-    // console.log(vornoiNodes);
-  
+    console.log('Nodes: ',vornoiNodes);
     // return (
     //   <div>
-    //     <div style={{ width: "800px", marginTop: "10px"}}>
-    //       <Select
-    //         components={makeAnimated()}
-    //         options={countries}
-    //         placeholder="Select a region"
-    //         onChange={setSelectedCountries}
-    //         isSearchable
-    //         isMulti
-    //       />
-    //     </div>
-  
-    //     <XYPlot xType="time" width={800} height={300}>
-    //       <VerticalGridLines />
-    //       <HorizontalGridLines />
-    //       <XAxis />
-    //       <YAxis />
-    //       {selectedCountries.map((d, index) => (
-    //         <LineSeries key={index} curve={"curveMonotoneX"} data={d.value} />
-    //       ))}
-  
-    //       {hoveredNode && <MarkSeries data={[hoveredNode]} />}
-    //       <Voronoi
-    //         nodes={vornoiNodes}
-    //         onHover={(node) => setHoveredNode(node)}
-    //         onBlur={() => setHoveredNode(null)}
-    //       />
-    //       <Crosshair
-    //         values={[hoveredNode]}
-    //         titleFormat={(d) => ({
-    //           title: d[0].country,
-    //           value: d[0].x.toString(),
-    //         })}
-    //         itemsFormat={(d) => [{ title: "Active Cases", value: d[0].y }]}
-    //       />
-    //     </XYPlot>
+    //     <p>
+    //       Fetched
+    //     </p>
     //   </div>
-    // );
+    // )
     return (
       <div>
-        <p>Data Fetched</p>
+        <div style={{ width: "800px", marginTop: "10px"}}>
+          <Select
+            components={makeAnimated()}
+            options={countries}
+            placeholder="Select a region"
+            onChange={setSelectedCountries}
+            isSearchable
+            isMulti
+          />
+        </div>
+  
+        <XYPlot xType="time" width={1080} height={800}>
+          <XAxis />
+          <YAxis />
+          {selectedCountries.map((d, index) => (
+            <LineSeries key={index} curve={"curveMonotoneX"} data={d.value} />
+          ))}
+  
+          {hoveredNode && <MarkSeries data={[hoveredNode]} />}
+          <Voronoi
+            nodes={vornoiNodes}
+            onHover={(node) => setHoveredNode(node)}
+            onBlur={() => setHoveredNode(null)}
+          />
+          <Crosshair
+            values={[hoveredNode]}
+            titleFormat={(d) => ({
+              title: d[0].country,
+              value: d[0].x.toString(),
+            })}
+            itemsFormat={(d) => [{ title: "Active Cases", value: d[0].y }]}
+          />
+        </XYPlot>
       </div>
-    )
+    );
   }
 }
