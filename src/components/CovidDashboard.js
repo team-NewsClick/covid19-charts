f096d716d375d8f147603b125bb73456592db757
MVS Sriharsha <sriharshamvs@gmail.com>
2020-11-17 19:28:04 +0530

    code refactor

--- src/components/CovidDashboard.js
+++ src/components/CovidDashboard.js
@@ -28,72 +28,61 @@ function fetchCovidData() {
   return data
 }
 
-function LineChartLinear(props) {
-  const data = props.data.data
+function LineChart(props) {
   const newCases = {
-    data: props.data.data,
+    data: null,
     lineLabel: 'New Cases',
     lineHeading: 'Graphical representation of New Cases of COVID-19',
-    scaleType: props.data.scaleType
+    scaleType: props.data.scaleType,
+    datesAdjusted: props.data.datesAdjusted
   }
-  if (data.length === 0) {
-    return (
-      <div>
-        <div className='m-3'>Loading...</div>
-      </div>
-    )
-  } else {
-    return <LineChartWidget data={newCases} />
-  }
-}
-
-function LineChartLog(props) {
-  const rawData = props.data.data
-  if (rawData.length === 0) {
-    return (
-      <div>
-        <div className='m-3'>Loading...</div>
-      </div>
-    )
-  } else {
-    const data = rawData.map((rows) => {
-      const countryData = rows.data.filter((row) => {
-        return row.y !== '0'
+  if (newCases.data === null) {
+    if (props.data.scaleType === 'log') {
+      const rawData = props.data.data
+      const data = rawData.map((rows) => {
+        const countryData = rows.data.filter((row) => {
+          return row.y !== '0'
+        })
+        const country = {
+          country: rows.country,
+          data: countryData
+        }
+        return country
       })
-      const country = {
-        country: rows.country,
-        data: countryData
-      }
-      return country
-    })
-    const newCases = {
-      data: data,
-      lineLabel: 'New Cases',
-      lineHeading: 'Graphical representation of New Cases of COVID-19',
-      scaleType: props.data.scaleType
+      newCases.data = data
+    } else {
+      const data = props.data.data
+      newCases.data = data
     }
+  }
+  if (newCases.data === null) {
+    return (
+      <div>
+        <div className='m-3'>Loading...</div>
+      </div>
+    )
+  } else {
     return <LineChartWidget data={newCases} />
   }
 }
 
 const CovidDashboard = () => {
-  
   const data = fetchCovidData()
 
   const [casesType, setCasesType] = useState('')
   const [dataType, setDataType] = useState('')
   const [scaleType, setScaleType] = useState('linear')
   const [datesAdjusted, setDatesAdjusted] = useState('')
-  
+
   const propsData = {
     data: data,
-    lineLabel: 'New Cases',
+    lineLabel: '',
     lineHeading: 'Graphical representation of New Cases of COVID-19',
     scaleType: scaleType,
     casesType: casesType,
-    datesAdjusted: datesAdjusted 
+    datesAdjusted: datesAdjusted
   }
-  
+
   return (
     <div>
       <div>
@@ -105,7 +94,7 @@ const CovidDashboard = () => {
               id='deaths'
               name='cases'
               value='deaths'
-              onChange={e => setCasesType(e.currentTarget.value)}
+              onChange={(e) => setCasesType(e.currentTarget.value)}
             />
             <label for='deaths'>Deaths</label>
             <input
@@ -114,7 +103,7 @@ const CovidDashboard = () => {
               name='cases'
               value='confirmed'
               checked='checked'
-              onChange={e => setCasesType(e.currentTarget.value)}
+              onChange={(e) => setCasesType(e.currentTarget.value)}
             />
             <label for='confirmed'>Confirmed</label>
           </div>
@@ -125,7 +114,7 @@ const CovidDashboard = () => {
               id='new'
               name='data-type'
               value='new'
-              onChange={e => setDataType(e.currentTarget.value)}
+              onChange={(e) => setDataType(e.currentTarget.value)}
             />
             <label for='new'>New</label>
             <input
@@ -134,7 +123,7 @@ const CovidDashboard = () => {
               name='data-type'
               value='cumulative'
               checked='checked'
-              onChange={e => setDataType(e.currentTarget.value)}
+              onChange={(e) => setDataType(e.currentTarget.value)}
             />
             <label for='cumulative'>Cumulative</label>
           </div>
@@ -146,7 +135,7 @@ const CovidDashboard = () => {
               name='display-type'
               value='log'
               checked={scaleType}
-              onChange={e => setScaleType(e.currentTarget.value)}
+              onChange={(e) => setScaleType(e.currentTarget.value)}
             />
             <label for='log'>Log</label>
             <input
@@ -155,7 +144,7 @@ const CovidDashboard = () => {
               name='display-type'
               value='linear'
               checked={scaleType}
-              onChange={e => setScaleType(e.currentTarget.value)}
+              onChange={(e) => setScaleType(e.currentTarget.value)}
             />
             <label for='linear'>Linear</label>
           </div>
@@ -166,7 +155,7 @@ const CovidDashboard = () => {
               id='yes'
               name='adjust-date'
               value='yes'
-              onChange={e => setDatesAdjusted(e.currentTarget.value)}
+              onChange={(e) => setDatesAdjusted(e.currentTarget.value)}
             />
             <label for='yes'>Yes</label>
             <input
@@ -175,13 +164,13 @@ const CovidDashboard = () => {
               name='adjust-date'
               value='no'
               checked='checked'
-              onChange={e => setDatesAdjusted(e.currentTarget.value)}
+              onChange={(e) => setDatesAdjusted(e.currentTarget.value)}
             />
             <label for='no'>No</label>
           </div>
         </div>
       </div>
-      {scaleType && scaleType == 'log' ? <LineChartLog data={propsData} /> : <LineChartLinear data={propsData} />}
+      <LineChart data={propsData} />
     </div>
   )
 }
