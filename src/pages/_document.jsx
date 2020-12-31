cae41fbf79b2a41ec64ca6af49753821fb742e8a
Bappa Sinha <bappasinha@gmail.com>
2020-12-31 11:54:38 +0530

    Add google Analytics tag for NewsClick.

---
+++ src/pages/_document.jsx
@@ -0,0 +1,33 @@
+import _Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class Document extends _Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+	  {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-38214496-1`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-38214496-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body className="bg-gray-100">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
