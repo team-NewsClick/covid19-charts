cfcf1a043a499429c027966052f5a4d3a560cbd3
iamsantoshah <s10santosh@yahoo.com>
2021-01-08 17:50:39 +0530

    article title components

---
+++ src/components/article/ArticleTitle.js
@@ -0,0 +1,12 @@
+const ArticleTitle = () => {
+  return (
+    <div
+      id="article-title"
+      className="text-4xl font-extrabold leading-10 mt-1 mb-8"
+    >
+      COVID-19 Cases: Data and Graphs of India and the World
+    </div>
+  )
+}
+
+export default ArticleTitle
