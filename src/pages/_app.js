e209770b0765e2a4d33517c2f250296986a3e495
harisai <harisai@newsclick.in>
2021-01-19 08:58:17 +0530

    added css to app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/index.css"
 import "../../node_modules/react-vis/dist/style.css"
 import "semantic-ui-css/semantic.min.css"
+import "mapbox-gl/dist/mapbox-gl.css"
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
