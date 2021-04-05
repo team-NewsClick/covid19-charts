8775d824376d4f0b15b750592f578f605c8bcf07
Santosh Shah <s10santosh@yahoo.com>
2021-04-05 12:36:04 +0530

    Testing: without radio buttons

--- src/pages/covid19-cases-graphs-maps-india-world.js
+++ src/pages/covid19-cases-graphs-maps-india-world.js
@@ -79,63 +79,39 @@ const Article = () => {
               className="mx-auto mt-16"
             ></iframe>
           </div>
-          {/* <StateVaccination className="mx-auto" /> */}
-          <div>
-            <div className="radio-toolbar m-2 flex justify-center">
-              <input
-                type="radio"
-                id="vaccination-chart"
-                name="vaccination-viz"
-                value="vaccination-chart"
-                defaultChecked
-                onChange={(e) => _handleVaccinationViz(e)}
-                />
-              <label htmlFor="vaccination-chart" className="vaccinationVizLabel">Vaccination Chart</label>
-              <input
-                type="radio"
-                id="vaccination-map"
-                name="vaccination-viz"
-                value="vaccination-map"
-                onChange={(e) => _handleVaccinationViz(e)}
-                />
-              <label htmlFor="vaccination-map" className="vaccinationVizLabel">Vaccination Map</label>
-              </div>
-              {vaccinationViz === "vaccination-chart"
-                ? <iframe
-                    id="cityTracker"
-                    src="/charts/VaccinationStateTracker"
-                    scrolling="no"
-                    frameBorder="0"
-                    width={windowWidth > 700 ? windowWidth * 0.67 : windowWidth * 0.95}
-                    height={windowWidth < 800
-                      ? windowWidth > 700
-                        ? windowWidth * 0.92
-                        : windowWidth * 1.6
-                      : windowWidth * 0.52
-                    }
-                    className="mx-auto mt-16"
-                  ></iframe>
-                : <iframe
-                    id="states-map"
-                    src="/maps/VaccinationStates"
-                    scrolling="no"
-                    frameBorder="0"
-                    width={windowWidth < 800
-                      ? windowWidth > 700
-                        ? windowWidth * 0.67
-                        : windowWidth * 0.9
-                      : windowWidth * 0.4
-                    }
-                    height={windowWidth < 800
-                      ? windowWidth > 700
-                        ? windowWidth * 0.8
-                        : windowWidth * 1.18
-                      : windowWidth * 0.48
-                    }
-                    className="mx-auto mt-16"
-                  ></iframe>
+          <iframe
+            id="cityTracker"
+            src="/charts/VaccinationStateTracker"
+            scrolling="no"
+            frameBorder="0"
+            width={windowWidth > 700 ? windowWidth * 0.67 : windowWidth * 0.95}
+            height={windowWidth < 800
+              ? windowWidth > 700
+                ? windowWidth * 0.92
+                : windowWidth * 1.6
+              : windowWidth * 0.52
+            }
+            className="mx-auto mt-16"
+          ></iframe>
+            <iframe
+              id="states-map"
+              src="/maps/VaccinationStates"
+              scrolling="no"
+              frameBorder="0"
+              width={windowWidth < 800
+                ? windowWidth > 700
+                  ? windowWidth * 0.67
+                  : windowWidth * 0.9
+                : windowWidth * 0.4
               }
-          </div>
+              height={windowWidth < 800
+                ? windowWidth > 700
+                  ? windowWidth * 0.8
+                  : windowWidth * 1.18
+                : windowWidth * 0.48
+              }
+              className="mx-auto mt-16"
+            ></iframe>
           <div className="article-para">
             The Table below shows the movement of the 7-day moving average of
             Daily Covid-19 cases, Tests conducted and Positivity Rate (daily
